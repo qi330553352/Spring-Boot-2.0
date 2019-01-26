fbdbd0db639b1e52bb97e5dcb5c949da8e9943e2
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2019-01-26 17:54:42 +0800

    添加 log4j2.xml 日志功能

--- chapter-mybatis-plugins/src/main/java/com/example/qixin/ChapterMybatisPluginsApplication.java
+++ chapter-mybatis-plugins/src/main/java/com/example/qixin/ChapterMybatisPluginsApplication.java
@@ -2,6 +2,7 @@ package com.example.qixin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 
